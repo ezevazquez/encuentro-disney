2ee134afe4997202e95141b8fd688cb4342465fe
Eze Vazquez <ezevazquez@MacBookPro.fibertel.com.ar>
2025-03-10 15:37:02 -0300

    mant page

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,35 @@
+
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+
+const MaintenancePage: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#D7CEC7]">
+      <motion.div
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="text-center p-8 bg-white bg-opacity-90 rounded-lg shadow-lg"
+      >
+        <motion.div
+          initial={{ scale: 0.8 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.6 }}
+          className="mb-6 flex justify-center"
+        >
+        </motion.div>
+        <h1 className="text-3xl font-semibold mb-4 text-[#333333]">
+          Estamos trabajando en nuestra web :)
+        </h1>
+        <p className="text-lg text-[#333333]">
+          Pronto volveremos con contenido mágico para vos y tu familia. 
+        </p>
+        <p>¡Gracias por tu paciencia!</p>
+      </motion.div>
+    </div>
+  );
+};
+
+export default MaintenancePage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,9 +8,22 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
-        pathname: '/images/**'
-      }
-    ]
-  }
+        pathname: '/images/**',
+      },
+    ],
+  },
+  async redirects() {
+    if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true') {
+      return [
+        {
+          source: '/((?!maintenance).*)',
+          destination: '/maintenance',
+          permanent: false,
+        },
+      ];
+    }
+    return [];
+  },
 };
+
 export default nextConfig;
